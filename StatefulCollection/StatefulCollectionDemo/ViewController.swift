6ce73f78a0019ca2ac63222a178c08020b2e7bed
yousefhamza <jo.adam.93@gmail.com>
2020-10-04 21:40:11 +0200

    Add demo app project

---
+++ StatefulCollection/StatefulCollectionDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  StatefulCollectionDemo
+//
+//  Created by Yousef Hamza on 10/4/20.
+//  Copyright © 2020 yousefhamz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
+
